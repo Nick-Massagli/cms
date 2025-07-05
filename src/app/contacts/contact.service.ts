41d461a7f1a32ebdcac60cfc4f2d87af549fd429
Nick-Massagli <mas19015@byui.edu>
2025-07-05 11:56:03 -0600

    Week 11 (canoot get to connect through nodemon)

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -8,27 +8,39 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ContactService {
+  // //variable for event emiter
   contactChangedEvent = new EventEmitter<Contact[]>();
 
+  //subject property
   contactListChangedEvent = new Subject<Contact[]>();
 
+  //property to hold max id
   maxContactId: number;
 
   contacts: Contact[] = [];
 
+  //inject http client
   constructor(private http: HttpClient) {
+    //init contact to be the ones coming from mockcontaccts
     // this.contacts = MOCKCONTACTS;
-    this.getContacts();
+    // this.getContacts();
+    //get the max id at init time
     this.maxContactId = this.getMaxId();
   }
 
+  //method to get all contaccts
   getContacts() {
-    this.http.get('https://nmasscms-default-rtdb.firebaseio.com/contacts.json')
+    //use http get
+    this.http.get<{ message: string, contacts: Contact[] }>('http://localhost:3000/contacts')
+      //subscribe to observable returning
       .subscribe(
-        (contacts: Contact[]) => {
-          this.contacts = contacts;
-          this.maxContactId = this.getMaxId();
+        //sucess function
+        (responseData) => {
+          //assign the array of contacts received to the contacts class attribute
+          this.contacts = responseData.contacts;
+          //sort alphabetically by name
           this.contacts.sort((a, b) => (a.name < b.name) ? 1 : (a.name > b.name) ? -1 : 0)
+          //signal that the list has changed
           this.contactListChangedEvent.next(this.contacts.slice());
         },
         (error: any) => {
@@ -37,7 +49,9 @@ export class ContactService {
       )
   }
 
-  getContact(id: string): Contact {
+  //method to get a single specific contact
+  getContact(id: string) {
+    // //loop through all the contacts
     // this.contacts.forEach(contact => {
     //   //if ids match
     //   if (contact.id === id) {
@@ -47,76 +61,139 @@ export class ContactService {
     // //if no id is found...
     // return null;
 
-    for (const contact of this.contacts) {
-      if (contact.id === id) {
-        return contact;
-      }
-    }
-    return null;
+    // for (const contact of this.contacts) {
+    //   if (contact.id === id) {
+    //     return contact;
+    //   }
+    // }
+    // return null;
+
+    return this.http.get<{ message: string, contact: Contact }>('http://localhost:3000/contacts/' + id);
   }
 
+  //method to get max id number in contact list
   getMaxId(): number {
+    //variable to hold max Id
     let maxId = 0;
+    //loop through the contact list
     for (const contact of this.contacts) {
+      //get current id as a number
       const currentId = +contact.id;
+      //if the current id is greater than max ID...
       if (currentId > maxId) {
+        //then max id is the current id
         maxId = currentId;
       }
     }
+    //return that max id
     return maxId;
   }
 
+  //method to add a contact when user press add button
   addContact(newContact: Contact) {
-    if (newContact === null || newContact === undefined) {
+    ////check if contact is defined
+    if (!newContact) {
+      //exit
       return;
     }
 
-    this.maxContactId++;
-    newContact.id = this.maxContactId.toString();
-    this.contacts.push(newContact);
-    this.storeContacts();
-  }
-
-  updateContact(originalContact: Contact, newContact: Contact) {
-    if (originalContact === null || originalContact === undefined || newContact === null || newContact === undefined) {
-      return;
-    }
-
-    const pos = this.contacts.indexOf(originalContact);
-    if (pos < 0) {
-      return;
-    }
-
-    newContact.id = originalContact.id;
-    this.contacts[pos] = newContact;
-    this.storeContacts();
-  }
-
-  deleteContact(contact: Contact) {
-    if (contact === null || contact === undefined) {
-      return;
-    }
-    const pos = this.contacts.indexOf(contact);
-
-    if (pos < 0) {
-      return;
-    }
-    this.contacts.splice(pos, 1);
-    this.storeContacts();
-  }
-
-  storeContacts() {
-    let contacts = JSON.stringify(this.contacts);
-
+    //set headers
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-    this.http.put('https://nmasscms-default-rtdb.firebaseio.com/contacts.json', contacts, { headers: headers })
+    //convert object to string to send in req
+    newContact.id = '';
+    const strContact = JSON.stringify(newContact);
+
+    //send req with object and headers
+    this.http.post('http://localhost:3000/contacts', strContact, { headers: headers })
+      //subscribe to response
       .subscribe(
-        () => {
-          this.contactListChangedEvent.next(this.contacts.slice());
-        }
-      )
+        (contacts: Contact[]) => {
+          //assign contact list
+          this.contacts = contacts;
+          //emit changes
+          this.contactChangedEvent.next(this.contacts.slice());
+        });
   }
+
+  //method to update/replace an existing contact
+  updateContact(originalContact: Contact, newContact: Contact) {
+    //check if contact or update is defined
+    if (!originalContact || !newContact) {
+      //exit
+      return;
+    }
+
+    //geet position in list of contacts
+    const pos = this.contacts.indexOf(originalContact);
+    //if position is not in array
+    if (pos < 0) {
+      //exit
+      return;
+    }
+
+    //set headers
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    //conver object to string to send in req
+    const strContact = JSON.stringify(newContact);
+
+    //send req with contact id, object and headers
+    this.http.patch('http://localhost:3000/contacts/' + originalContact.id
+      , strContact
+      , { headers: headers })
+      //subscribe to response
+      .subscribe(
+        (contacts: Contact[]) => {
+          //assign contacts list
+          this.contacts = contacts;
+          //emit changes
+          this.contactChangedEvent.next(this.contacts.slice());
+        });
+  }
+
+  //method to delete a contact
+  deleteContact(contact: Contact) {
+    //check if contact is undefined
+    if (!contact) {
+      //exit
+      return;
+    }
+
+    //send request with specific id
+    this.http.delete('http://localhost:3000/contacts/' + contact.id)
+      //subscribe to response
+      .subscribe(
+        (contacts: Contact[]) => {
+          //assing list of contacts
+          this.contacts = contacts;
+          //emit changes
+          this.contactChangedEvent.next(this.contacts.slice());
+        });
+  }
+
+  //method to store contacts in database with put request
+  // storeContacts() {
+  //   //stringify the list of documnts
+  //   let contacts = JSON.stringify(this.contacts);
+
+  //   //create header for content type
+  //   const headers = new HttpHeaders({
+  //     'Content-Type': 'application/json'
+  //   });
+
+  //   //put method with url, contacts object to replace, and headers
+  //   this.http.put('https://cms-app-d5fce.firebaseio.com/contacts.json', contacts, { headers: headers })
+  //     //subscribe to response
+  //     .subscribe(
+  //       () => {
+  //         //once a response has been received, signal that the document list has changed, send copy of list
+  //         this.contactListChangedEvent.next(this.contacts.slice());
+  //       }
+  //     )
+  // }
 }
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -7,26 +7,41 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class DocumentService {
+export class DocumentsService {
   documents: Document[] = [];
+  // //event emiter for changes in the document
+  // documentChangedEvent = new EventEmitter<Document[]>();
+
+  //property for max id
   maxDocumentId: number;
+
+  //subject property
   documentListChangedEvent = new Subject<Document[]>();
 
   constructor(private http: HttpClient) {
+    //init documents to be the ones coming from mock
     // this.documents = MOCKDOCUMENTS;
-    this.getDocuments();
-    this.maxDocumentId = this.getMaxId();
+    // this.getDocuments();
+    //get the max id at init time
+    // this.maxDocumentId = this.getMaxId();
   }
 
+  //method to get all documents
   getDocuments() {
+    // //return a copy of the array of documents
     // return this.documents.slice();
 
-    this.http.get('https://nmasscms-default-rtdb.firebaseio.com/documents.json')
+    //use http get
+    this.http.get<{ message: string, documents: Document[] }>('http://localhost:3000/documents')
+      //subscribe to observable returning
       .subscribe(
-        (documents: Document[]) => {
-          this.documents = documents;
-          this.maxDocumentId = this.getMaxId();
+        //sucess function
+        (documentData) => {
+          //assign the array of documents received to the documents class attribute
+          this.documents = documentData.documents;
+          //sort alphabetically by name
           this.documents.sort((a, b) => (a.name < b.name) ? 1 : (a.name > b.name) ? -1 : 0)
+          // signal that the list has changed
           this.documentListChangedEvent.next(this.documents.slice());
         },
         (error: any) => {
@@ -35,6 +50,7 @@ export class DocumentService {
       )
   }
 
+  //method to get a single specific document
   getDocument(id: string): Document {
     for (const document of this.documents) {
       if (document.id === id) {
@@ -44,67 +60,129 @@ export class DocumentService {
     return null;
   }
 
+  //method to get max id number in document list
   getMaxId(): number {
+    //variable to hold max Id
     let maxId = 0;
+    //loop through the document list
     for (const document of this.documents) {
+      //get current id as a number
       const currentId = +document.id;
-
+      //if the current id is greater than max ID...
       if (currentId > maxId) {
+        //then max id is the current id
         maxId = currentId;
       }
     }
+    //return that max id
     return maxId;
   }
 
-  addDocument(newDocument: Document) {
-    if (newDocument === null || newDocument === undefined) {
+  //method to add a document when user press add button
+  addDocument(document: Document) {
+    //check if document is defined
+    if (!document) {
+      //if so, exit function
       return;
     }
 
-    this.maxDocumentId++;
-    newDocument.id = this.maxDocumentId.toString();
-    this.documents.push(newDocument);
-    this.storeDocuments();
-  }
-
-  updateDocument(originalDocument: Document, newDocument: Document) {
-    if (originalDocument === null || originalDocument === undefined || newDocument === null || newDocument === undefined) {
-      return;
-    }
-
-    const pos = this.documents.indexOf(originalDocument);
-    if (pos < 0) {
-      return;
-    }
-    newDocument.id = originalDocument.id;
-    this.documents[pos] = newDocument;
-    this.storeDocuments();
-  }
-
-  deleteDocument(document: Document) {
-    if (document === null || document === undefined) {
-      return;
-    }
-    const pos = this.documents.indexOf(document);
-    
-    if (pos < 0) {
-      return;
-    }
-    this.documents.splice(pos, 1);
-    this.storeDocuments();
-  }
-
-  storeDocuments() {
-    let documents = JSON.stringify(this.documents);
+    //create header
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-    this.http.put('https://nmasscms-default-rtdb.firebaseio.com/documents.json', documents, { headers: headers })
+    //convert object to string to send on request
+    document.id = '';
+    const strDocument = JSON.stringify(document);
+
+    //send post request with document and header
+    this.http.post('http://localhost:3000/documents', strDocument, { headers: headers })
+      //subscribe to response
       .subscribe(
-        () => {
+        (documents: Document[]) => {
+          //assign document list
+          this.documents = documents;
+          //emit the change
           this.documentListChangedEvent.next(this.documents.slice());
-        }
-      )
+        });
   }
+
+  //method to update/replace an existing document
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    //check if documents are undefined
+    if (!originalDocument || !newDocument) {
+      //if so, exit
+      return;
+    }
+
+    //get position of original document
+    const pos = this.documents.indexOf(originalDocument);
+    //if the position is not in the array
+    if (pos < 0) {
+      //exit
+      return;
+    }
+
+    //set headers
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    //convert document object to string
+    const strDocument = JSON.stringify(newDocument);
+
+    //send patch request with original document id, new document object and headers
+    this.http.patch('http://localhost:3000/documents/' + originalDocument.id
+      , strDocument
+      , { headers: headers })
+      //subscribe to response
+      .subscribe(
+        (documents: Document[]) => {
+          //assign updated document list
+          this.documents = documents;
+          //emit change
+          this.documentListChangedEvent.next(this.documents.slice());
+        });
+  }
+
+  //method to delete a document via request to server
+  deleteDocument(document: Document) {
+    //check if documents are undefined
+    if (document === null || document === undefined) {
+      //exit
+      return;
+    }
+
+    //send request to delete using document id in params
+    this.http.delete('http://localhost:3000/documents/' + document.id)
+      //subscribe to response
+      .subscribe(
+        (documents: Document[]) => {
+          //assign modified document list
+          this.documents = documents;
+          //emit changes
+          this.documentListChangedEvent.next(this.documents.slice());
+        });
+  }
+
+  //method to store documents in database with put request
+  // storeDocuments() {
+  //   //stringify the list of documnts
+  //   let documents = JSON.stringify(this.documents);
+
+  //   //create header for content type
+  //   const headers = new HttpHeaders({
+  //     'Content-Type': 'application/json'
+  //   });
+
+  //   //put method with url, documents object to replace, and headers
+  //   this.http.put('https://cms-app-d5fce.firebaseio.com/documents.json', documents, { headers: headers })
+  //     //subscribe to response
+  //     .subscribe(
+  //       () => {
+  //         //once a response has been received, signal that the document list has changed, send copy of list
+  //         this.documentListChangedEvent.next(this.documents.slice());
+  //       }
+  //     )
+  // }
 }
